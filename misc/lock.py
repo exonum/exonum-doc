4771d68ed0dd93a2822384569b5cf561f7ec6d9c
Alex Ostrovski <ostrovski.alex@gmail.com>
2017-09-10 20:40:47 +0300

    Add license info to locking script

--- misc/lock.py
+++ misc/lock.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python
 
+# Script allowing to execute one or more linters.
+# Runs from the project root directory.
+# It is assumed that the HTML pages are already built and are located in $SRC_DIR.
+
+# Copyright 2017 The Exonum Team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
 Script that transforms the output of `pip freeze` to contain only dependencies
 related to a particular requirements file.
