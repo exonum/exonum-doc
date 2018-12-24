2451794164fbd6a6d4536dfde3b395a5fbe3f7bc
SergNikolaev <iintersergey@gmail.com>
2018-12-24 13:49:57 +0200

    regexp fix

--- theme/assets/js/index.js
+++ theme/assets/js/index.js
@@ -1,14 +1,11 @@
-'use strict';
+'use strict'
 
 window.addEventListener('load', function () {
-  var versionSelect = document.getElementById('version-select');
-  var regexp = /version\/.+\//;
-  versionSelect.value = location.pathname
-    .match(regexp)[0]
-    .replace('version', '')
-    .replace(/\//g, '');
+  var versionSelect = document.getElementById('version-select')
+  var regexp = /version\/(.+?)\//
+  versionSelect.value = location.pathname.match(regexp)[1]
 
   versionSelect.addEventListener('change', function () {
-    window.location.replace(location.href.replace(regexp, 'version/' + this.value));
-  });
-});
+    window.location.replace(location.href.replace(regexp, 'version/' + this.value + '/'))
+  })
+})
